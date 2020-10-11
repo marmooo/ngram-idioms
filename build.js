d3aa240e13e5e79c6b3382eb1caa1c18a3bb8f99
marmooo <marmooo@users.noreply.github.com>
2020-10-11 21:46:03 +0900

    support level=10

--- build.js
+++ build.js
@@ -1,6 +1,6 @@
 const execSync = require('child_process').execSync;
 
-for (var i=1; i<=9; i++) {
+for (var i=1; i<=10; i++) {
   execSync(`node kanji-words.js ${i} > dist/${i}.lst`);
   execSync(`node minify.js ${i} > dist/${i}.arr`);
 }
