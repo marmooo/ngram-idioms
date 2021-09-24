c3732989482a319fa531a41716ed68ccb260c307
marmooo <marmooo@users.noreply.github.com>
2021-09-25 00:00:30 +0900

    cleanup

--- build.js
+++ build.js
@@ -2,5 +2,4 @@ const execSync = require('child_process').execSync;
 
 for (var i=1; i<=10; i++) {
   execSync(`node kanji-words.js ${i} > dist/${i}.lst`);
-  execSync(`node minify.js ${i} > dist/${i}.arr`);
 }
